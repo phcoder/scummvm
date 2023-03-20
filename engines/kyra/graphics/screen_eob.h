bafdb625338814741d3f0f5c4b4967b1bfa0f78b
Vladimir Serbinenko <phcoder@gmail.com>
2023-03-20 15:04:32 +0100

    KYRA: Support compression used in Chinese eob2

--- engines/kyra/graphics/screen_eob.h
+++ engines/kyra/graphics/screen_eob.h
@@ -67,6 +67,7 @@ public:
 
 	void printShadedText(const char *string, int x, int y, int col1, int col2, int shadowCol, int pitch = -1);
 
+	void loadChineseEOB2LZBitmap(Common::SeekableReadStream *s, int pageNum, uint32 size);
 	void loadBitmap(const char *filename, int tempPage, int dstPage, Palette *pal, bool skip = false) override;
 	void loadEoBBitmap(const char *file, const uint8 *cgaMapping, int tempPage, int destPage, int convertToPage);
 	void loadShapeSetBitmap(const char *file, int tempPage, int destPage);
