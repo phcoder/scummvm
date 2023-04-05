0b458fbc4bf71b97f34478878570a4052ee9b905
Vladimir Serbinenko <phcoder@gmail.com>
2023-04-05 20:51:35 +0200

    BIOS Tall decrease height

--- engines/kyra/graphics/screen_eob.cpp
+++ engines/kyra/graphics/screen_eob.cpp
@@ -2011,8 +2011,8 @@ bool OldDOSFont::loadPCBIOSTall() {
 	_width = 8;
 	const int originalBytesPerGlyph = 8;
 	const int originalHeight = 8;
-	const int bytesPerGlyph = 15;
-	_height = originalHeight * 2 - 1;
+	const int bytesPerGlyph = 14;
+	_height = originalHeight * 2 - 2;
 	_data = new uint8[_numGlyphs * bytesPerGlyph + _numGlyphs * sizeof(uint16)];
 	assert(_data);
 
@@ -2022,7 +2022,8 @@ bool OldDOSFont::loadPCBIOSTall() {
 		_bitmapOffsets[i] = _numGlyphs * sizeof(uint16) + i * bytesPerGlyph;
 		byte *optr = _data + _bitmapOffsets[i];
 		const byte *iptr = Graphics::DosFont::fontData_PCBIOS + i * originalBytesPerGlyph;
-		for (int j = 0; j < originalHeight - 1; j++) {
+		*optr++ = *iptr++;
+		for (int j = 1; j < originalHeight - 1; j++) {
 			*optr++ = *iptr;
 			*optr++ = *iptr++;
 		}
